3a328313676d0c47e7c52b6a6c65951f713d5ef9
emir91 <vranacemir@gmail.com>
2020-09-29 12:55:52 +0200

    syntax fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,15 +28,15 @@ submit_message = function(){
     if(e.keyCode == 13) {
       $('button').click()
       e.target.value = ""
-    }
-  })
-}
+    };
+  });
+};
 
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
-  submit_message()
-  scroll_bottom()
-})
+  submit_message();
+  scroll_bottom();
+});
