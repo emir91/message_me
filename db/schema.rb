f2b0ca8a1005b2d6f1ff62b9c74eb0c1c2931513
emir91 <vranacemir@gmail.com>
2020-09-30 15:29:23 +0200

    add signup funcionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_082118) do
+ActiveRecord::Schema.define(version: 2020_09_30_125035) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_082118) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
