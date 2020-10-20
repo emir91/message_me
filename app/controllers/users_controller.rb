2d3a9f3ebeb711884028b9bbb450683a1f599643
emir91 <vranacemir@gmail.com>
2020-10-20 11:30:13 +0200

    add edit and update actions to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only:[:edit, :update]
   
     def new
       @user = User.new
@@ -15,10 +17,26 @@ class UsersController < ApplicationController
       end
     end
 
+    def edit
+    end
+
+    def update
+      if @user.update(user_params)
+        flash[:notice] = "Profile successfully updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
     private
 
     def user_params
      params.require(:user).permit(:email,:username,:password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 
 end
